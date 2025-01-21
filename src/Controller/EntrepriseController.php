69e39539c33c8fc57aec2ba0a14914c2b1a8b3f2
Estherlvn <estherlvn@gmail.com>
2025-01-21 09:21:49 +0100

    méthode controller entreprise

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Entreprise;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 final class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('entreprise/index.html.twig', [
-            'controller_name' => 'EntrepriseController',
+        $entreprises = $entityManager->getRepository(Entreprise::class)->finAll();
+        return $this->render('entreprise/index.html.twig',[
+            'entreprises' => $entreprises
+       
         ]);
     }
 }
