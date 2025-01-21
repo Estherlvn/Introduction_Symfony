963159f513f4976982b51c5b3290645120e12e48
Estherlvn <estherlvn@gmail.com>
2025-01-21 11:48:05 +0100

    Methodes et vues : affichages des données -entreprise -employe (.twig et controllers)

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Entreprise;
+use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,10 +13,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 final class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(EntityManagerInterface $entityManager): Response
+    // public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
     {
-        $entreprises = $entityManager->getRepository(Entreprise::class)->finAll();
-        return $this->render('entreprise/index.html.twig',[
+
+        // $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+        //  $entreprises = $entrepriseRepository->findAll();
+        // SELECT * FROM entreprise WERE ville = 'Strasbourg' ORDER BY raisonSociale ASC
+        $entreprises = $entrepriseRepository->findBy(["ville" => "STRASBOURG"], ["raisonSociale" => "ASC"]);
+        return $this->render('entreprise/index.html.twig', [
             'entreprises' => $entreprises
        
         ]);
